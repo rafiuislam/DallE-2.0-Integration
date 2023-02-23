e8595c406041e41dcbc93e387fdc055742992e29
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2023-02-23 16:14:31 +0600

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { logo } from "./assets";
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">
+      <img :src="logo" class="logo" alt="" />
+    </RouterLink>
+    <RouterLink to="/create-post" class="link"> Create </RouterLink>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
